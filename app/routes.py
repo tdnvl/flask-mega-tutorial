079fd953b3b23523a5ab5a6d6f25df2486c2a69c
tdnvl <bummel1889@gmail.com>
2018-06-03 21:38:30 -0400

    Add the Eplore page.

--- app/routes.py
+++ app/routes.py
@@ -24,16 +24,7 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
+    posts = current_user.followed_posts().all()
     return render_template("index.html", title='Home Page', form=form, posts=posts)
 
 
@@ -133,3 +124,11 @@ def unfollow(username):
     db.session.commit()
     flash('You are not following {}.'.format(username))
     return redirect(url_for('user', username=username))
+
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
+
